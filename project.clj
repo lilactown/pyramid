1c8775b3968ae27bf2b097cf1be488905a1e8120
Will Acton <will@willacton.com>
2020-12-12 12:58:52 -0800

    project.clj

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject lilactown/autonormal "1.0.1"
+  :description "A library for storing and querying graph data in a Clojure map"
+  :url "https://github.com/lilactown/autonormal"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v20.html"}
+  :source-paths ["src"]
+  :dependencies [[edn-query-language/eql "1.0.1"]]
+  :deploy-repositories [["snapshots" {:sign-releases false :url "https://clojars.org"}]])
