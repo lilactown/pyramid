6d21253d538f910eba2bae40f83a089363493da2
Will Acton <will@willacton.com>
2021-10-27 12:59:53 -0700

    3.1.0-alpha

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject town.lilac/pyramid "3.0.0"
+(defproject town.lilac/pyramid "3.1.0-alpha"
   :description "A library for storing and querying graph data in a Clojure map"
   :url "https://github.com/lilactown/pyramid"
   :license {:name "Eclipse Public License"
