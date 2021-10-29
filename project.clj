367af36e8c62a2fc0f1921377ed3320bfd6dbc52
Will Acton <will@willacton.com>
2021-10-29 11:06:48 -0700

    bump AGAIN

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject town.lilac/pyramid "3.1.1"
+(defproject town.lilac/pyramid "3.1.2"
   :description "A library for storing and querying graph data in a Clojure map"
   :url "https://github.com/lilactown/pyramid"
+  :scm {:name "git" :url "https://github.com/lilactown/pyramid"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
   :source-paths ["src"]
