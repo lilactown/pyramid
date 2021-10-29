47d7adfa077a54e2a58f97c6f20e522cd9e1cc9c
Will Acton <will@willacton.com>
2021-10-29 10:37:04 -0700

    include fast-zip as dependency :face-palm:

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject town.lilac/pyramid "3.1.0"
+(defproject town.lilac/pyramid "3.1.1"
   :description "A library for storing and querying graph data in a Clojure map"
   :url "https://github.com/lilactown/pyramid"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v20.html"}
   :source-paths ["src"]
-  :dependencies [[edn-query-language/eql "1.0.1"]]
+  :dependencies [[edn-query-language/eql "1.0.1"]
+                 [fast-zip/fast-zip "0.7.0"]]
   :deploy-repositories [["snapshots" {:sign-releases false
                                       :url "https://clojars.org"
                                       :creds :gpg}]])
